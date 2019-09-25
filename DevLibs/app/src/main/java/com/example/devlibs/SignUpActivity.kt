259bff671a66e9e73cfd426e8e7e5f315f560444
Troy Christian <apostasy8@gmail.com>
2019-09-24 23:59:52 -0400

    add StorySelection activity.

--- DevLibs/app/src/main/java/com/example/devlibs/SignUpActivity.kt
+++ DevLibs/app/src/main/java/com/example/devlibs/SignUpActivity.kt
@@ -1,12 +1,25 @@
 package com.example.devlibs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up)
+
+        create_account.text = intent.getStringExtra("LOGIN_INTENT_CODE")
+
+        create_account.setOnClickListener{
+            if(username_field_signin.text.toString() != "" && password_field_signin.text.toString() != "") {
+                val goToMode = Intent(this, StorySelection::class.java)
+                startActivity(goToMode)
     }
-}
+    // temp function for project build out.
+
+}}}
+
+
---
+++ DevLibs/app/src/main/java/com/example/devlibs/StorySelection.kt
@@ -0,0 +1,12 @@
+package com.example.devlibs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StorySelection : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story_selection)
+    }
+}
