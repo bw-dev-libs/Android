9bb71f06448ffdbd56b2012af41827e85fdbbbe6
Troy Christian <apostasy8@gmail.com>
2019-09-25 01:46:22 -0400

    add intent to take user to word input activity

--- DevLibs/app/src/main/java/com/example/devlibs/StorySelection.kt
+++ DevLibs/app/src/main/java/com/example/devlibs/StorySelection.kt
@@ -1,12 +1,18 @@
 package com.example.devlibs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_story_selection.*
 
 class StorySelection : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_story_selection)
+        intent = Intent(this, programmerStoryWordSelection::class.java)
+        funny_programmer.setOnClickListener{
+            startActivity(intent)
+        }
     }
 }
---
+++ DevLibs/app/src/main/java/com/example/devlibs/programmerStoryWordSelection.kt
@@ -0,0 +1,12 @@
+package com.example.devlibs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class programmerStoryWordSelection : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_programmer_story_word_selection)
+    }
+}
